bba95777ec77f64dca08790fa4faff286542267c
SomratIIT <101197949+SomratIIT@users.noreply.github.com>
2024-11-04 17:56:16 +0600

    Task 03-04

--- js_part1_solve.js
+++ js_part1_solve.js
@@ -1,6 +1,6 @@
 //  Task 02
 
-function rpt(arr)
+/* function rpt(arr)
 {
 let count = 0;
 
@@ -18,5 +18,49 @@ const final = rpt( [5,6,11,12,98, 5]);
 
 
 
+console.log(final); */
+
+
+// Task 03
+
+/* function vow(word){
+    let count=0;
+    const vowels=['a','e','i','o','u']
+for(let i=0 ;i<word.length;i++)
+{
+   if(vowels.includes(word[i]))
+   
+    {
+        count++;
+    }
+}
+return count;
+}
+
+const final = vow('Hellooo');
+
 console.log(final);
+ */
+
+// task 04
+
+/* function sample(str){
+const value = str.split(' ');
+let max = value[0];
+console.log(value);
+// return value[4].length;
+for(let i=1 ;i<value.length;i++){
+    if(max.length<=value[i].length)
+    {
+        max = value[i];
+    }
+   
+}
+
+return max;
+
+}
+const final = sample(' I am learning Programming to become a programmer');
+
+console.log(final); */
 
